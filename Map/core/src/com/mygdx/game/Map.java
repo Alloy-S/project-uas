688e54b58a6337eb23fee9fe22726dec7727e75e
novemeliaw <107181236+novemeliaw@users.noreply.github.com>
2022-06-18 14:39:26 +0700

    Add files via upload

--- Map/core/src/com/mygdx/game/Map.java
+++ Map/core/src/com/mygdx/game/Map.java
@@ -1,9 +1,6 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.*;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -12,16 +9,15 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.maps.MapObjects;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
-public class Map extends ApplicationAdapter implements InputProcessor {
+public class Map extends ApplicationAdapter implements InputProcessor{
     TiledMap map;
     OrthographicCamera camera;
-    TiledMapRenderer mapRenderer;
+    OrthogonalTiledMapRenderer mapRenderer;
     Player player;
     private Assets assets;
     private Skin skin;
@@ -132,8 +128,6 @@ public class Map extends ApplicationAdapter implements InputProcessor {
                 player.setAbleToMove(false);
             }
         }
-
-
     }
 
     @Override
